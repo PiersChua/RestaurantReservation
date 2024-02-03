b50b265552b67fbc011074fbc85845d1b92edaf7
PiersChua <pierschua@gmail.com>
2024-02-03 23:48:36 +0800

    added logic to limit the number of reservations the user can reserve. finished project ;)

--- RestaurantReservation/Shared/Domain/Favourite.cs
+++ RestaurantReservation/Shared/Domain/Favourite.cs
@@ -9,7 +9,6 @@ namespace RestaurantReservation.Shared.Domain
 	public class Favourite
 	{
 		public int Id { get; set; }
-		//public string? Name { get; set; }
 		public int? RestaurantId { get; set; }
 		public virtual Restaurant? Restaurant { get; set; }
 		public int? CustomerId { get; set; }
